c53cedcad1681ecb9e29bf04ce03fde76fa6bcbc
rixrax <rixrax@users.noreply.github.com>
2017-11-23 19:16:16 +0200

    Added vim config.

--- install.sh
+++ install.sh
@@ -15,3 +15,21 @@ if [[ ! -L "$HOME_DIR/.gitconfig" ]]; then
     ln -sn "$DOTFILES_DIR/git/.gitconfig" "$HOME_DIR/.gitconfig"
 fi
 
+#Vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+if [[ -f "$HOME_DIR/.vimrc" && ! -L "$HOME_DIR/.vimrc" ]]; then
+    #Backup vim config file
+    #Notify user
+    echo "Found existing vim config file, moving it to $DOTFILES_DIR/backup/"
+    cp "$HOME_DIR/.vimrc" "$DOTFILES_DIR/backup/.vimrc"
+    rm "$HOME_DIR/.vimrc"
+fi
+
+if [[ ! -L "$HOME_DIR/.vimrc" ]]; then
+    #Create symlink
+    ln -sn "$DOTFILES_DIR/vim/vimrc" "$HOME_DIR/.vimrc"
+fi
+vim +PlugInstall +qall
+
